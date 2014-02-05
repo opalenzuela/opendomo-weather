b9bd3d7223435fc43c4b9af7b71a3d2066878380
opalenzuela <opalenzuela@gmail.com>
2014-02-05 17:19:03 +0100

    Cleanup and adjustments!

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 VERSION=`date '+%Y%m%d'`
-ARCH=noarch
 USR="--owner 1000 --group 1000 --same-permissions "
 EXCLUDE=" --exclude '*~' --exclude .svn "
-PKGNAME="odweather"
+PKGID="odweather"
 
 rm -fr pkg/*.tar.gz
-tar vcfz ./pkg/$PKGNAME-$VERSION.od.$ARCH.tar.gz usr var $USR $EXCLUDE
+tar vcfz ./pkg/$PKGID-$VERSION.tar.gz usr var $USR $EXCLUDE
 
 
