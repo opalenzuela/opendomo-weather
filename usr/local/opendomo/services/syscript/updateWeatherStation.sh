1075fe0cd2035c45b2cf81c7914848afc953acb2
opalenzuela <opalenzuela@gmail.com>
2014-02-10 15:02:14 +0100

    Stats removed from the script. New variables
    Stats removed from the script. New variables like humidity, wind speed
    and description

--- usr/local/opendomo/services/syscript/updateWeatherStation.sh
+++ usr/local/opendomo/services/syscript/updateWeatherStation.sh
@@ -8,12 +8,7 @@ LOCALFILE="/var/opendomo/tmp/wundstation.tmp"
 URL="http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml"
 DATADIR="/var/opendomo/control/wundstation"
 CONFDIR="/etc/opendomo/control/wundstation"
-STATSDIR="/var/opendomo/log/stats"
 
-if ! test -d "$STATSDIR"
-then
-	mkdir -p $STATSDIR 
-fi
 
 if test -f "$CFGFILE"; then
 	. $CFGFILE
@@ -35,11 +30,36 @@ if test -f "$CFGFILE"; then
 			echo "tags='climate'" >> $CONFDIR/pressure.info
 			echo "units='mb'" >> $CONFDIR/pressure.info
 			echo "type='analog'" >> $CONFDIR/pressure.info
+			
+			echo "way=in" > $CONFDIR/wind.info
+			echo "status=enabled" >> $CONFDIR/wind.info
+			echo "name='Wind speed'" >> $CONFDIR/wind.info
+			echo "tags='climate'" >> $CONFDIR/wind.info
+			echo "units='mph'" >> $CONFDIR/wind.info
+			echo "type='analog'" >> $CONFDIR/wind.info			
+
+			echo "way=in" > $CONFDIR/humidity.info
+			echo "status=enabled" >> $CONFDIR/humidity.info
+			echo "name='Humidity'" >> $CONFDIR/humidity.info
+			echo "tags='climate'" >> $CONFDIR/humidity.info
+			echo "units='%'" >> $CONFDIR/humidity.info
+			echo "type='analog'" >> $CONFDIR/humidity.info	
+			
+			echo "way=in" > $CONFDIR/description.info
+			echo "status=enabled" >> $CONFDIR/description.info
+			echo "name='Description'" >> $CONFDIR/description.info
+			echo "tags='climate'" >> $CONFDIR/description.info
+			echo "units='%'" >> $CONFDIR/description.info
+			echo "type='text'" >> $CONFDIR/description.info				
 		fi
 
 		if test -f "$LOCALFILE"; then
 			t=`grep temp_c $LOCALFILE | sed 's/[^0-9.]//g'`
 			p=`grep pressure_mb $LOCALFILE | sed 's/[^0-9.]//g'`
+			w=`grep wind_mph $LOCALFILE | sed 's/[^0-9.]//g'`
+			h=`grep relative_humidity $LOCALFILE | sed 's/[^0-9.]//g'`
+			d=`grep "<weather>" $LOCALFILE | cut -f2 -d'>' | cut -f1 -d'<'`
+			
 			H=`date +%H`
 			DATE=`date +%s`
 			rm $LOCALFILE
@@ -51,8 +71,9 @@ if test -f "$CFGFILE"; then
 
 			echo "$t" > $DATADIR/temp
 			echo "$p" > $DATADIR/pressure
-			echo "$DATE $t" >> $STATSDIR/wundstation-temp.h$H
-			echo "$DATE $p" >> $STATSDIR/wundstation-pressure.h$H		
+			echo "$w" > $DATADIR/wind
+			echo "$h" > $DATADIR/humidity
+			echo "$d" > $DATADIR/description			
 		fi
 	else
 		rm $DATADIR/*
